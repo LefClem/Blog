0483d4bbf42cb19830b38e3b1d35b56b9815b3bb
Clement <clement@Clements-MacBook-Pro.local>
2024-03-25 23:52:42 +0100

    add docker to CI + update test

--- backend/src/services/user.service.spec.ts
+++ backend/src/services/user.service.spec.ts
@@ -1,14 +1,19 @@
-import { User } from "../entities/user";
 import * as userService from "../services/user.service";
 
-// const signUpMock = jest.spyOn(User.prototype, 'save').mockImplementation()
 jest.mock('../services/user.service', () => ({
   login: jest.fn().mockImplementation((email: string, password: string) => {
-    if(email === 'invalid@mail.com' && password === 'wrongpassword'){
+    if(email === 'invalid@mail.com' || !email && password === 'wrongpassword' || !password){
         return false
     } else {
         return 'mocked-token';
     }
+  }),
+  signup: jest.fn().mockImplementation((email: string, password: string) => {
+    if( !email || !password){
+      return false
+  } else {
+      return 'mocked-user';
+  }
   })
 }));
 
@@ -19,8 +24,31 @@ describe("User service", () => {
     const password = 'test';
 
     const result = await userService.login(email, password);    
-    console.log(result);
         
     expect(result).toBeDefined();
+  })
+
+  it("should return an error with incorrect login infos", async () => {
+    const email = '';
+    const password = '';
+
+    const result = await userService.login(email, password);    
+    expect(result).toBe(false);
+  })
+
+  it("should return a new user", async () => {
+    const email = 'test02@mail.com';
+    const password = '1234';
+
+    const result = await userService.signup(email, password);
+    expect(result).toBe('mocked-user');
+  })
+
+  it("should return an error if an info is wrong", async () => {
+    const email = '';
+    const password = '1234';
+
+    const result = await userService.signup(email, password);
+    expect(result).toBe(false);
   })
 });
